678ff5dfa70df4f60ad0e221efc0dfaba860d725
Gray Leonard <gl7039a@american.edu>
2018-08-06 23:34:47 -0400

    changed htpasswd

--- squid_script.sh
+++ squid_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-USERNAME=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+#USERNAME=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+USERNAME="august"
 PASS=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 
-yum install squid wget httpd-tools -y &&
-touch /etc/squid/passwd &&
-htpasswd -b /etc/squid/passwd ${USERNAME} ${PASS} &&
-wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/grayleonard/squid-remote-install/master/squid.conf --no-check-certificate &&
-touch /etc/squid/blacklist.acl &&
+yum install squid wget httpd-tools -y
+htpasswd -cb /etc/squid/passwd ${USERNAME} ${PASS}
+wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/grayleonard/squid-remote-install/master/squid.conf --no-check-certificate
+touch /etc/squid/blacklist.acl
 systemctl restart squid.service && systemctl enable squid.service
 IPADDR=$( hostname -I | xargs )
 echo ${IPADDR}:3128:${USERNAME}:${PASS}
